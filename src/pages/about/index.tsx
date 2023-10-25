8f2cded4e0d448df426fabfbc276f5de96c154a9
ostapvasilenko <>
2023-10-25 20:03:04 +0300

    add first commit

---
+++ src/pages/about/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Link from 'next/link'
+
+const index = () => {
+  return ( 
+    <div>
+        About
+        <Link href="/">Home</Link>
+        <a href="/">Home</a>
+    </div>
+  )
+}
+
+export default index
