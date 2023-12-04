ea200fe08d205d8caee2f819d2649bd18648b2f7
ilovefish3801 <pipipupi.kom@gmail.com>
2023-12-04 18:51:44 +0200

    a

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -1,14 +1,40 @@
-import React from 'react'
-import Link from 'next/link'
+import { Component, ReactNode } from "react";
 
-const index = () => {
-  return ( 
-    <div>
-        About
-        <Link href="/">Home</Link>
-        <a href="/">Home</a>
-    </div>
-  )
+interface State {
+  counter: number
 }
 
-export default index
+interface Props { }
+
+class About extends Component<Props, State>{
+  constructor(props: Props) {
+    super(props)
+    this.state = {
+      counter: 0
+    }
+  }
+
+  increment = () => {
+    this.setState((prevState) => ({
+      counter: prevState.counter + 1
+    }))
+  }
+  decrement = () => {
+    this.setState((prevState) => ({
+      counter: prevState.counter - 1
+    }))
+  }
+
+  render(): ReactNode {
+    return (
+      <>
+        <h2>About page: {this.state.counter}</h2>
+        <button onClick={this.increment}>+</button>
+        <button onClick={this.decrement}>-</button>
+      </>
+    )
+  }
+}
+
+
+export default About
